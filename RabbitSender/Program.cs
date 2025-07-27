fb23a2ca5ab68cad253030bea3cba0d8084553f6
lkarzhalovi <karzhalovi@gmail.com>
2025-07-27 17:18:52 +0400

    add RabbitMQ Sender

---
+++ RabbitSender/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+ConnectionFactory factory = new()
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672"),
+    ClientProvidedName = "Rabbit Sender App"
+};
+
+var cnn =  await factory.CreateConnectionAsync(cancellationToken: CancellationToken.None);
+
+var channel =  await cnn.CreateChannelAsync();
+
+var exchangeName = "DemoExchange";
+var routeKey = "demo-route-key";
+var queueName = "DemoQueue";
+
+await channel.ExchangeDeclareAsync(exchangeName, ExchangeType.Direct);
+await channel.QueueDeclareAsync(queueName, false,false,false);
+await channel.QueueBindAsync(queueName, exchangeName, routeKey);
+
+
+var message = Encoding.UTF8.GetBytes("Hello Luka!");
+await channel.BasicPublishAsync(exchangeName,routeKey,message);
+
+await channel.CloseAsync();
+await cnn.CloseAsync();
